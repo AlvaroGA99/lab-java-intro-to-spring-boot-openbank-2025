121092e928c4b257e596def07dab9b7fc77db6e5
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-04-24 15:01:38 +0200

    Initial commit

---
+++ src/main/java/com/ironhack/lab_java_intro_springboot/LabJavaIntroSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab_java_intro_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaIntroSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJavaIntroSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/lab_java_intro_springboot/LabJavaIntroSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab_java_intro_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJavaIntroSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
